31d106374a0f03cfa8d5f0ce29caa46878941685
aibbya <aibbyale@gmail.com>
2020-08-28 00:06:56 -0300

    template y mapa implementado

---
+++ public/javascripts/map.js
@@ -0,0 +1,9 @@
+var mymap = L.map("main_map").setView([-34.5608648, -58.4972602], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "https://www.openstreetmap.org",
+}).addTo(mymap);
+
+L.marker([-34.559098, -58.4957157]).addTo(mymap);
+L.marker([-34.5577394, -58.4962143]).addTo(mymap);
+L.marker([-34.5563071, -58.4965326]).addTo(mymap);
