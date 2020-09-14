df28aef7da6ce4abbf7098a75df98befdc7d3471
aibbya <aibbyale@gmail.com>
2020-09-14 16:27:24 -0300

    agregando seguridad, token

--- models/mdlUsuarios.js
+++ models/mdlUsuarios.js
@@ -1,10 +1,51 @@
 var mongoose = require("mongoose");
 var Reserva = require("./mdlReserva");
+const { NotExtended } = require("http-errors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 var Schema = mongoose.Schema;
 
+const validateEmail = (email) => {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+  return re.test(email);
+};
+
 var usuarioSchema = new Schema({
-  nombre: String,
+  nombre: {
+    type: String,
+    trim: true,
+    required: [true, "El nombre es obligatorio"],
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "El email es obligatorio"],
+    lowercase: true,
+    unique: 
+    validate: [validateEmail, "Por favor ingrese un email valido"],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/],
+
+  },
+  password: {
+    type: String,
+    required: [true, "El password es obligatorio"],
+  },
+  passwordResetToken: String,
+  passwordResetTokenExpire: Date,
+  verificate: {
+    type: Boolean,
+    default: false,
+  },
 });
+usuarioSchema.plugin(uniqueVadator, {message: 'El {PATH} ya existe con otro usuario'})
+
+usuarioSchema.pre("save"),
+  function () {
+    if (this.isModified("password")) {
+      this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+  };
 
 usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
   var reserva = new Reserva({
---
+++ models/mdltoken.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TokenSchema = new Schema ({
+  _userId: {
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true,
+    ref: 'User'
+   },
+   token: {
+     type: String,
+     required: true
+   },
+   createAt: {
+     type: Date,
+     required:true,
+     default: Date.now,
+     expired: 43200
+   }
+
+})
+
+module.exports = mongoose.model('Token', TokenSchema)
