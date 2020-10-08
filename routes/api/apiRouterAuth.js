346fd82e16497e3de8e97c857eb2b78e4956c9cc
aibbya <aibbyale@gmail.com>
2020-10-08 00:10:31 -0300

    facebook

--- routes/api/apiRouterAuth.js
+++ routes/api/apiRouterAuth.js
@@ -1,11 +1,16 @@
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 
-const authController = require('../../controllers/api/authCtrApi');
+const authController = require("../../controllers/api/authCtrApi");
+const passport = require("passport");
 
-router.post('/authenticate', authController.authenticate);
-router.post('forgotPassword', authController.forgotPassword);
-router.post('/facebook_token', passport.authenticate('facebook-token'), authController.authFacebookToken);
+router.post("/authenticate", authController.authenticate);
+router.post("forgotPassword", authController.forgotPassword);
+router.post(
+  "/facebook_token",
+  passport.authenticate("facebook-token"),
+  authController.authFacebookToken
+);
 
 module.exports = router;
